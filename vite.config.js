b1087ac83fdf6be1199c7585a11caef0e82fad21
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2024-01-23 20:03:33 +0200

    fix redirect 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rental-car-project/',
+  base: '/rental-car-project',
   plugins: [react()],
 });
